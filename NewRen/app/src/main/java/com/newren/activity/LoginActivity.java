b83bda29e49f8c975c4c580e7e9018f2ce2e2f03
applenele <1941973283@qq.com>
2015-08-03 10:13:39 +0800

    login 跳转

---
+++ NewRen/app/src/main/java/com/newren/activity/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.newren.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.newren.R;
+
+public class LoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+
+}
--- NewRen/app/src/main/java/com/newren/activity/MainActivity.java
+++ NewRen/app/src/main/java/com/newren/activity/MainActivity.java
@@ -4,16 +4,32 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.newren.R;
 
 
 public class MainActivity extends Activity {
 
+    Button btnLogin = null;
+    Button btnRegister =null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnLogin =(Button)findViewById(R.id.btnLogin);
+        btnRegister =(Button)findViewById(R.id.btnRegister);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast tst = Toast.makeText(MainActivity.this, "111111111", Toast.LENGTH_SHORT);
+                tst.show();
+            }
+        });
     }
 
 

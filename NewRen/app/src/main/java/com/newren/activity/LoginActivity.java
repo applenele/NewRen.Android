7127295cf7f3883d2f98226a5e78f7f82dce1370
applenele <1941973283@qq.com>
2015-08-03 10:55:11 +0800

    跳转

--- NewRen/app/src/main/java/com/newren/activity/LoginActivity.java
+++ NewRen/app/src/main/java/com/newren/activity/LoginActivity.java
@@ -1,18 +1,59 @@
 package com.newren.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.newren.R;
 
 public class LoginActivity extends Activity {
 
+    Button btnBack = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btnBack = (Button)findViewById(R.id.btnBack);
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                String value = intent.getStringExtra("activity");
+                try{
+                    intent.setClass(LoginActivity.this, Class.forName("com.newren.activity." + value));
+                    startActivity(intent);
+                    finish();
+                }
+                catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if(keyCode == KeyEvent.KEYCODE_BACK){
+            Intent intent = getIntent();
+            String value = intent.getStringExtra("activity");
+            try{
+                intent.setClass(LoginActivity.this, Class.forName("com.newren.activity." + value));
+                startActivity(intent);
+                finish();
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+        return super.onKeyDown(keyCode, event);
     }
 
 
--- NewRen/app/src/main/java/com/newren/activity/MainActivity.java
+++ NewRen/app/src/main/java/com/newren/activity/MainActivity.java
@@ -1,6 +1,7 @@
 package com.newren.activity;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -26,8 +27,12 @@ public class MainActivity extends Activity {
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast tst = Toast.makeText(MainActivity.this, "111111111", Toast.LENGTH_SHORT);
-                tst.show();
+                /*Toast tst = Toast.makeText(MainActivity.this, "111111111", Toast.LENGTH_SHORT);
+                tst.show();*/
+                Intent intent = new Intent();
+                intent.putExtra("activity", "MainActivity");
+                intent.setClass(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
             }
         });
     }
